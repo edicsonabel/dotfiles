4dfe1c48c55ee98f85e05390d9e3962cfe420445
Edicson Abel <edicsonabelinfo@gmail.com>
2023-05-15 12:26:15 -0400

    refactor: improvement and order in aliases

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -26,35 +26,26 @@ oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
 <# Plugin #>
 . (Join-Path (Get-ScriptDirectory) "plugins\git.plugin.ps1")
 
-<# Alias #>
-function cleanMemory { 
-  [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
-  rm (Get-PSReadlineOption).HistorySavePath
-}
+<# Linux aliases #>
 function gitDir { $gitDir=$(which git).Replace('\cmd\git.EXE', '').Replace('\cmd\git.exe', ''); echo "$gitDir\usr\bin" }
-function lsGit { & ls.exe --color $args}
-Set-Alias ls lsGit
-function diffGit { & diff.exe $args}
+function diffGit { & "$(gitDir)\diff.exe" $args}
 del alias:diff -Force
 Set-Alias diff diffGit
-function echoGit { & echo.exe $args}
+function echoGit { & echo.exe $args} # don't add $(gitDir)
 del alias:echo -Force
 Set-Alias echo echoGit
 function findGit { & "$(gitDir)\find.exe" $args}
 # del alias:find -Force
 Set-Alias find findGit
+function lsGit { & "$(gitDir)\ls.exe" --color $args}
+Set-Alias ls lsGit
+function l { & "$(gitDir)\ls.exe" --color $args}
+function ll { & "$(gitDir)\ls.exe" --color -l $args}
+function la { & "$(gitDir)\ls.exe" --color -la $args}
+function lh { & "$(gitDir)\ls.exe" --color -lh $args}
+function lha { & "$(gitDir)\ls.exe" --color -lha $args}
 
-function l { & ls $args}
-function ll { & ls -l $args}
-function la { & ls -la $args}
-function lh { & ls -lh $args}
-function lha { & ls -lha $args}
-function htop { & ntop $args}
-function .. { & cd ..}
-
-function projects { & cd $($Env:PROJECTS)}
-
-# NPM
+<# NPM aliases #>
 function na { & npm add $args }
 function ni { & npm install $args }
 function nr { & npm remove $args }
@@ -62,7 +53,8 @@ function ns { & npm start $args }
 function nu { & npm up $args }
 function ninit { & npm init $args }
 function nrun { & npm run $args }
-# YARN
+
+<# YARN aliases #>
 function ya { & yarn add $args }
 function yi { & yarn install $args }
 function yr { & yarn remove $args }
@@ -70,7 +62,8 @@ function yu { & yarn up $args }
 function ys { & yarn start $args }
 function yinit { & yarn init $args }
 function yrun { & yarn run $args }
-# PNPM
+
+<# PNPM aliases #>
 function pna { & pnpm add $args }
 function pni { & pnpm install $args }
 function pnr { & pnpm remove $args }
@@ -79,11 +72,19 @@ function pns { & pnpm start $args }
 function pninit { & pnpm init $args }
 function pnrun { & pnpm run $args }
 
+<# Customized aliases #>
+function cleanMemory { 
+  [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
+  rm (Get-PSReadlineOption).HistorySavePath
+}
+function htop { & ntop $args}
+function .. { & cd ..}
+function projects { & cd $($Env:PROJECTS)}
 function pwdFn { & Get-Location | Foreach-Object { $_.Path } }
 Set-Alias pwd pwdFn
 Set-Alias vim nvim
 Set-Alias cat bat  
 # Set-Alias ls lsd
 
-# CLEAR TERMINAL
+<# Clear terminal #>
 cls
